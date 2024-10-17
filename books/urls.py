9df459d0b9f184659023fe35cf87f5b95011ec4f
Alexandr Politov <apolitov83@gmail.com>
2024-10-17 19:10:13 +0300

    BookRetrieveSerializer updated to show authos' names and genres' names

--- books/urls.py
+++ books/urls.py
@@ -4,9 +4,9 @@ from rest_framework import routers
 from books.views import GenreViewSet, AuthorViewSet, BookViewSet
 
 router = routers.DefaultRouter()
+router.register("", BookViewSet),
 router.register("genres", GenreViewSet),
 router.register("authors", AuthorViewSet),
-router.register("books", BookViewSet),
 
 
 app_name = "books-service"
