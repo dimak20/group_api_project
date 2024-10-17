334a9cc8f467d487395b192da787c2769239a20f
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-10-17 10:41:06 +0300

    Models Book Author Genre created

--- books/models.py
+++ books/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Book(models.Model):
+    COVER = [("Hard", "Hard"), ("Soft", "Soft")]
+
+    title = models.CharField(max_length=255)
+    authors = models.ManyToManyField(Author, related_name="books")
+    genres = models.ManyToManyField(Genre, related_name="books")
+    cover = models.CharField(choices=COVER, default="Hard", max_length=4)
+    inventory = models.PositiveIntegerField(default=0)
+    daily_fee = models.DecimalField(decimal_places=2, max_digits=5, default=0)
