f2873dc7492a20ae39bfb3b16dc52eeb80c6151c
Ivan Shakhman <shakhmanivan@gmail.com>
2024-10-17 17:29:47 +0300

    add action method to return book

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,9 @@
+from django.core.serializers import get_serializer
+from django.db import transaction
 from django.utils import timezone
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from checkout.models import Checkout
 from checkout.serializers import (
@@ -49,3 +53,26 @@ class CheckoutViewSet(viewsets.ModelViewSet):
             actual_return_date=timezone.now()
         )
 
+    @action(
+        methods=("POST",),
+        detail=True,
+        permission_classes=(permissions.IsAuthenticated,),
+        url_path="return"
+    )
+    def return_book(self, request, *args, **kwargs):
+        checkout = self.get_object()
+        book = checkout.book
+        if checkout.actual_return_date:
+            return Response(
+                {
+                    "Return error": "this book is already returned"
+                }
+            )
+        with transaction.atomic():
+            book.inventory += 1
+            book.save()
+            checkout.save()
+
+        serializer = self.get_serializer(checkout, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
