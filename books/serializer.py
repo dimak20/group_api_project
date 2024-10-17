3c26ab2a9c8c4468721d2a98337a40cb34716032
Alexandr Politov <apolitov83@gmail.com>
2024-10-17 15:58:14 +0300

    serializers for 'books' app created

---
+++ books/serializer.py
@@ -0,0 +1,56 @@
+from rest_framework import serializers
+
+from books.models import Author, Genre, Book
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ["id", "name"]
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ["id", "first_name", "last_name"]
+
+
+class AuthorListRetrieveSerializer(AuthorSerializer):
+    books = serializers.SlugRelatedField(many=True, read_only=True, slug_field="name")
+
+    class Meta(AuthorSerializer.Meta):
+        fields = AuthorSerializer.Meta.fields + ["books"]
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = (
+            "id",
+            "title",
+            "authors",
+            "genres",
+            "cover",
+            "inventory",
+            "daily_fee",
+            "image"
+        )
+
+
+class BookListSerializer(BookSerializer):
+    authors = serializers.SerializerMethodField()
+    genres = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="name"
+    )
+
+    class Meta(BookSerializer.Meta):
+        fields = ["id", "title", "authors", "genres", "image"]
+
+    def get_authors(self, obj):
+        authors = obj.authors.all()
+        return [f"{author}" for author in authors]
+
+
+class BookRetrieveSerializer(BookSerializer):
+    authors = serializers.ManyRelatedField(many=True, read_only=True)
+    genres = serializers.ManyRelatedField(many=True, read_only=True)
