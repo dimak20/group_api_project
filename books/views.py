823990d16b5c5665ffeaca9349879d90597c455c
Alexandr Politov <apolitov83@gmail.com>
2024-10-17 16:06:20 +0300

    ModelViewSets created for 'books' app

--- books/views.py
+++ books/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from books.models import Genre, Author, Book
+from books.serializer import GenreSerializer, AuthorSerializer, AuthorListRetrieveSerializer, BookListSerializer, \
+    BookRetrieveSerializer, BookSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return AuthorListRetrieveSerializer
+        return AuthorSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action in ["list", "retrieve"]:
+            queryset = queryset.prefetch_related("books")
+        return queryset
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookListSerializer
+        elif self.action == "retrieve":
+            return BookRetrieveSerializer
+        return BookSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action in ["list", "retrieve"]:
+            queryset = queryset.prefetch_related("authors", "genres")
+        return queryset
