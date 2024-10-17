785c8942b4058ef67854af8f79d92b7da07ae84a
Ivan Shakhman <shakhmanivan@gmail.com>
2024-10-17 16:51:49 +0300

    create viewset and implement get_serializer_class method

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from tkinter.tix import CheckList
 
-# Create your views here.
+from django.shortcuts import render
+from rest_framework import viewsets
+
+from checkout.models import Checkout
+
+
+class CheckoutViewSet(viewsets.ModelViewSet):
+    queryset = Checkout.objects.all()
+
+    def get_serializer_class(self):
+
+        if self.action == "list":
+            return ChekoutListSerializer
+        if self.action == "retrieve":
+            return CheckoutDetailSerializer
+
+        return CheckoutSerializer
