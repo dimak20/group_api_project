e6e9c8913cd1c6c64a8c89c020e732297910119f
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-10-17 10:40:29 +0300

    Modify celery settings

---
+++ group_api_library/__init__.py
@@ -0,0 +1,3 @@
+from group_api_library.celery import app as celery_app
+
+__all__ = ["celery_app"]
--- group_api_library/celery.py
+++ group_api_library/celery.py
@@ -2,9 +2,9 @@ import os
 from celery import Celery
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "group_library_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "group_api_library.settings")
 
-app = Celery("group_library_project")
+app = Celery("group_api_library")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
