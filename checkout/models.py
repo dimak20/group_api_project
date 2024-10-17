a20387024addf6b6033d789abf2023196d5e6fc7
Denys Piuro <denys.pyuro@gmail.com>
2024-10-17 14:48:39 +0300

    Create Checkout model

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models import DO_NOTHING
 
-# Create your models here.
+
+class Checkout(models.Model):
+    checkout_date = models.DateTimeField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book = models.ForeignKey(Book, on_delete=DO_NOTHING, related_name="checkouts")
+    user = models.ForeignKey(User, on_delete=DO_NOTHING, related_name="checkouts")
+
+    def __str__(self) -> str:
+        return (
+            f"Expected checkout duration - {self.expected_return_date - self.borrow_date}"
+        )
