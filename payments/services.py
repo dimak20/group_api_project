afc157b2139653db3ca673f0bbf35f2829ce087e
DSahalatyi <dsahalatyi@gmail.com>
2024-10-18 10:25:12 +0300

    Add views, services & urls

---
+++ payments/services.py
@@ -0,0 +1,94 @@
+from decimal import Decimal
+
+import stripe
+from django.urls import reverse
+from rest_framework.request import Request
+
+from checkout.models import Checkout
+from payments.exceptions import InvalidPeriodError
+from payments.models import Payment
+
+
+def create_checkout_session(checkout_id: int, request: Request):
+    checkout = Checkout.objects.filter(id=checkout_id).first()
+    if not checkout:
+        return None, "Checkout does not exist"
+
+    try:
+        billing_period = get_billing_period(checkout, overdue=False)
+        return create_stripe_checkout_session(billing_period, checkout, request)
+    except InvalidPeriodError as e:
+        return None, str(e)
+
+
+def create_overdue_payment(checkout_id: int, request: Request):
+    checkout = Checkout.objects.filter(id=checkout_id).first()
+    if not checkout:
+        return None, "Checkout does not exist"
+
+    try:
+        billing_period = get_billing_period(checkout, overdue=True)
+        return create_stripe_checkout_session(billing_period, checkout, request)
+    except InvalidPeriodError as e:
+        return None, str(e)
+
+
+def create_stripe_checkout_session(billing_period, checkout, request):
+    total_amount = int(
+        (Decimal(billing_period) * checkout.book.daily_fee * Decimal("100")).quantize(
+            Decimal("1")
+        )
+    )
+
+    if total_amount <= 0:
+        return None, "Invalid amount to pay."
+
+    success_url_base = request.build_absolute_uri(reverse("payments:success-url"))
+    success_url = f"{success_url_base}?session_id={{CHECKOUT_SESSION_ID}}"
+
+    cancel_url = request.build_absolute_uri(reverse("payments:cancel-url"))
+
+    checkout_session = stripe.checkout.Session.create(
+        payment_method_types=["card"],
+        line_items=[
+            {
+                "price_data": {
+                    "currency": "usd",
+                    "product_data": {
+                        "name": checkout.book.title,
+                    },
+                    "unit_amount": total_amount,
+                },
+                "quantity": 1,
+            }
+        ],
+        mode="payment",
+        success_url=success_url,
+        cancel_url=cancel_url,
+    )
+
+    payment = Payment.objects.create(
+        status="pending",
+        payment_type="payment",
+        checkout=checkout,
+        session_url=checkout_session.url,
+        session_id=checkout_session.id,
+        total_amount=total_amount,
+    )
+    return payment, checkout_session.url
+
+
+def get_billing_period(checkout: Checkout, overdue: bool = False):
+    if overdue:
+        borrow_days_of_overdue = (
+            checkout.actual_return_date - checkout.expected_return_date
+        ).days
+        if borrow_days_of_overdue < 0:
+            return None, "Return date cannot be before borrow date."
+
+        return borrow_days_of_overdue
+
+    borrow_duration_days = (checkout.expected_return_date - checkout.checkout_date).days
+    if borrow_duration_days < 0:
+        raise InvalidPeriodError("Return date cannot be before checkout date.")
+    return borrow_duration_days
--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,150 @@
-from django.shortcuts import render
+import logging
+from datetime import timedelta
+from decimal import Decimal
 
-# Create your views here.
+import stripe
+from django.conf import settings
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
+from django.utils import timezone
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from drf_spectacular.utils import extend_schema
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from checkout.models import Checkout
+from payments.models import Payment, StatusChoices
+from payments.serializers import (
+    PaymentListSerializer,
+    PaymentDetailSerializer,
+    PaymentSerializer,
+    BorrowingIDSerializer,
+)
+
+logger = logging.getLogger(__name__)
+
+DOMAIN = settings.HOME_DOMAIN
+
+WEBHOOK_SECRET = settings.WEBHOOK_SECRET
+
+stripe.api_key = settings.STRIPE_TEST_SECRET_KEY
+
+FINE_MULTIPLIER = 2
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    model = Payment
+    queryset = Payment.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PaymentListSerializer
+
+        if self.action == "retrieve":
+            return PaymentDetailSerializer
+
+        return PaymentSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action in ["list", "retrieve"] and not self.request.user.is_staff:
+            return queryset.filter(
+                borrowing__user__id=self.request.user
+            ).select_related()
+
+        if self.action in ["list", "retrieve"]:
+            return self.queryset.select_related()
+
+        return queryset
+
+
+@extend_schema(
+    request=BorrowingIDSerializer,
+    responses={
+        201: PaymentSerializer,
+        404: "Borrowing not found",
+        400: "Bad request",
+    },
+)
+class CreateCheckoutSessionView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = BorrowingIDSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        borrowing_id = serializer.validated_data["checkout_id"]
+        try:
+            borrowing = Checkout.objects.get(id=borrowing_id)
+            borrow_duration_days = 3
+            money_to_pay = int(
+                (
+                        Decimal(borrow_duration_days)
+                        * borrowing.book.daily_fee
+                        * Decimal("100")
+                ).quantize(Decimal("1"))
+            )
+            checkout_session = stripe.checkout.Session.create(
+                payment_method_types=["card"],
+                line_items=[
+                    {
+                        "price_data": {
+                            "currency": "usd",
+                            "product_data": {
+                                "name": borrowing.book.title,
+                            },
+                            "unit_amount": money_to_pay,
+                        },
+                        "quantity": 1,
+                    }
+                ],
+                mode="payment",
+                success_url=DOMAIN + "/api/v1/payments/success/?session_id={CHECKOUT_SESSION_ID}",
+                cancel_url=DOMAIN + "/apu/v1/payments/cancel/",
+            )
+            payment = Payment.objects.create(
+                status="pending",
+                payment_type="payment",
+                checkout=borrowing,
+                session_url=checkout_session.url,
+                session_id=checkout_session.id,
+                money_to_pay=money_to_pay,
+            )
+            serializer = PaymentSerializer(payment)
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except Checkout.DoesNotExist:
+            return JsonResponse(
+                {"error": "Borrowing not found."}, status=status.HTTP_404_NOT_FOUND
+            )
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CancelPaymentView(APIView):
+    def get(self, request, *args, **kwargs):
+        available_until = timezone.now() + timedelta(hours=24)
+
+        message = {
+            "message": "You can paid for this borrowing in 24 hours",
+            "available_until": available_until.isoformat()
+        }
+
+        return Response(message, status=status.HTTP_200_OK)
+
+
+class SuccessPaymentView(APIView):
+    def get(self, request, *args, **kwargs):
+        session_id = request.query_params.get("session_id")
+
+        if not session_id:
+            return Response("Stripe has failed to provide session id.", status=400)
+
+        payment = get_object_or_404(Payment, session_id=session_id)
+        payment.status = StatusChoices.PAID
+        payment.save()
+
+        return Response("Your payment was successful!", status=status.HTTP_200_OK)
