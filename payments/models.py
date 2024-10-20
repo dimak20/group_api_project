46631b1cd085eafc57b1691297b413a8f08ef2c6
DSahalatyi <dsahalatyi@gmail.com>
2024-10-20 17:32:11 +0300

    Add shortened url & update serializers

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,4 @@
+import pyshorteners
 from django.db import models
 from django.db.models import CASCADE
 
@@ -39,7 +40,12 @@ class Payment(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def short_url(self):
+        shorterner = pyshorteners.Shortener()
+        return shorterner.tinyurl.short(self.session_url)
+
     def __str__(self) -> str:
-        return (
-            f"Status: {self.status}. URL: {self.session_url}."
-        )
+        if self.status in [StatusChoices.PAID, StatusChoices.EXPIRED]:
+            return f"ID: {self.id}. Status: {self.status}."
+        return f"Status: {self.status}. URL: {self.short_url}."
