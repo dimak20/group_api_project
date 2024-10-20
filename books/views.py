c7de1bf1eef767c9a77ebe3cdeeedff9f78c7e96
Alexandr Politov <apolitov83@gmail.com>
2024-10-20 11:41:28 +0300

    'books' app. model 'Genres'. added field 'description'.

--- books/views.py
+++ books/views.py
@@ -6,6 +6,7 @@ from books.filters import BookFilter
 from books.models import Genre, Author, Book
 from books.serializers import (
     GenreSerializer,
+    GenreRetrieveSerializer,
     AuthorSerializer,
     AuthorListRetrieveSerializer,
     BookSerializer,
@@ -17,7 +18,11 @@ from books.schemas.books_filtering_ordering import book_list_create_schema
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
-    serializer_class = GenreSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return GenreRetrieveSerializer
+        return GenreSerializer
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
