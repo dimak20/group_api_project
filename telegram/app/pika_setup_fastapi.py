1ad2042d8b9d148f816c252d936339fabefa28f5
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-10-25 03:16:10 +0300

    Trying to implement rabbitMQ as a broker manager

--- telegram/app/pika_setup_fastapi.py
+++ telegram/app/pika_setup_fastapi.py
@@ -44,22 +44,29 @@ async def send_message(message: str):
 async def callback(message: aio_pika.IncomingMessage):
     async with message.process():
         print(f"Received: {message.body.decode()}")
-        await bot.send_message(message.body.decode())
+        await bot.send_message(758342560, message.body.decode())
 
 
 async def start_rabbit_consumer():
-    try:
-        connection = await connect_to_rabbitmq()
-        async with connection:
-            channel = await connection.channel()
+    while True:  # Запускаем бесконечный цикл для попытки переподключения
+        try:
+            connection = await connect_to_rabbitmq()
+            async with connection:
+                channel = await connection.channel()
 
-            # Установите QoS, если нужно
-            await channel.set_qos(prefetch_count=1)  # Ограничивает количество необработанных сообщений
-            payments_queue = await channel.declare_queue(PAYMENTS_QUEUE, durable=True)
+                # Установите QoS, если нужно
+                await channel.set_qos(prefetch_count=1)  # Ограничивает количество необработанных сообщений
+                payments_queue = await channel.declare_queue(PAYMENTS_QUEUE, durable=True)
 
-            # Подписка на очередь PAYMENTS
-            await payments_queue.consume(callback)
-            print("Consumer started, waiting for messages...")
-            await asyncio.Future()  # Ожидание сообщений бесконечно
-    except Exception as e:
-        print("Error in RabbitMQ consumer:", e)
+                # Подписка на очередь PAYMENTS
+                await payments_queue.consume(callback)
+                print("Consumer started, waiting for messages...")
+                await asyncio.Future()  # Ожидание сообщений бесконечно
+
+        except (aio_pika.exceptions.AMQPConnectionError, aio_pika.exceptions.ChannelClosed) as e:
+            print("Connection lost, retrying in 5 seconds...")
+            await asyncio.sleep(5)  # Подождите 5 секунд перед следующей попыткой
+
+        except Exception as e:
+            print("Error in RabbitMQ consumer:", e)
+            await asyncio.sleep(5)  # Подождите перед следующей попыткой
